bf43ee54331ee7afafe3bad0851973c5a9bee511
Pedro <pbruno1283@gmail.com>
2021-10-27 19:53:21 -0300

    Initial commit

---
+++ app/src/main/java/com/pedrobruno/meditationui/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.pedrobruno.meditationui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.pedrobruno.meditationui.ui.theme.MeditationUiTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MeditationUiTheme {
+
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/pedrobruno/meditationui/ui/HomeScreen.kt
@@ -0,0 +1,2 @@
+package com.pedrobruno.meditationui.ui
+
