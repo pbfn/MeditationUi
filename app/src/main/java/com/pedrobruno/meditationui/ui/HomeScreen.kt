9c5807f3cddd04df5389b93727bc43ba03f79b57
Pedro <pbruno1283@gmail.com>
2021-10-27 20:06:02 -0300

    GreetingSection add in HomeScreen

--- app/src/main/java/com/pedrobruno/meditationui/ui/HomeScreen.kt
+++ app/src/main/java/com/pedrobruno/meditationui/ui/HomeScreen.kt
@@ -1,2 +1,61 @@
 package com.pedrobruno.meditationui.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.pedrobruno.meditationui.R
+import com.pedrobruno.meditationui.ui.theme.DeepBlue
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier
+            .background(DeepBlue)
+            .fillMaxSize()
+    ) {
+        GreetingSection()
+    }
+}
+
+
+@Composable
+fun GreetingSection(
+    name: String = "Pedro"
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Good morning, $name",
+                style = MaterialTheme.typography.h2
+            )
+            Text(
+                text = "We wish you have a good day",
+                style = MaterialTheme.typography.body1
+            )
+        }
+        Icon(
+            painter = painterResource(id = R.drawable.ic_search),
+            contentDescription = "Search",
+            tint = Color.White,
+            modifier = Modifier.size(24.dp)
+
+        )
+
+    }
+}
