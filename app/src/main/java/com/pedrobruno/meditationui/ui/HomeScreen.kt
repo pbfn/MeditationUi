a206166a4cd15ce809681349e5ebb78ac75914e9
Pedro <pbruno1283@gmail.com>
2021-10-27 20:35:45 -0300

    CurrentMeditation add in HomeScreen

--- app/src/main/java/com/pedrobruno/meditationui/ui/HomeScreen.kt
+++ app/src/main/java/com/pedrobruno/meditationui/ui/HomeScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
@@ -20,10 +21,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.pedrobruno.meditationui.R
-import com.pedrobruno.meditationui.ui.theme.ButtonBlue
-import com.pedrobruno.meditationui.ui.theme.DarkerButtonBlue
-import com.pedrobruno.meditationui.ui.theme.DeepBlue
-import com.pedrobruno.meditationui.ui.theme.TextWhite
+import com.pedrobruno.meditationui.ui.theme.*
 
 @Composable
 fun HomeScreen() {
@@ -33,7 +31,8 @@ fun HomeScreen() {
             .fillMaxSize()
     ) {
         GreetingSection()
-        ChipSection(chips = listOf("Sweet sleep","Insomnia","Depression"))
+        ChipSection(chips = listOf("Sweet sleep", "Insomnia", "Depression"))
+        CurrentMeditation()
     }
 }
 
@@ -96,5 +95,49 @@ fun ChipSection(chips: List<String>) {
                 Text(text = chips[it], color = TextWhite)
             }
         }
+    }
+}
+
+@Composable
+fun CurrentMeditation(color: Color = LightRed) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .padding(15.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(color)
+            .padding(horizontal = 15.dp, vertical = 20.dp)
+            .fillMaxWidth()
+    ) {
+        Column {
+            Text(
+                text = "Daily Thought",
+                style = MaterialTheme.typography.h2
+            )
+            Text(
+                text = "Meditation • 3-10 min",
+                style = MaterialTheme.typography.body1,
+                color = TextWhite
+            )
+        }
+
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .background(ButtonBlue)
+                .padding(10.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_play),
+                contentDescription = "Play",
+                tint = Color.White,
+                modifier = Modifier.size(16.dp)
+            )
+        }
+
+
     }
 }
